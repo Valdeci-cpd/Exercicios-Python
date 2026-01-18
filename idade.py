3d577669990b5781a69a25727df05bfa4bbc98e9
Valdeci-cpd <valdeci@sergranbel.com.br>
2026-01-18 16:44:43 -0300

    Add files via upload

---
+++ idade.py
@@ -0,0 +1,11 @@
+ano = int(input("Digite o ano de nascimento: "))
+
+idade = 2025 - ano
+
+dias = idade * 365
+
+horas = dias * 24
+
+print(f"Você tem {idade} anos de idade.")
+print(f"Você já viveu {dias} dias.")
+print(f"Você viveu {horas} horas até agora.")
